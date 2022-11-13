eda5b3a1eb265e0725c405413fa24b5ee35f734c
olesiadrv <115192323+olesiadrv@users.noreply.github.com>
2022-11-13 16:06:19 +0200

    homework 27.10

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let obj = {1: 'one',2: 'two',3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 0: 'zero'};
+
+function num(array) {
+  return array.map(i => obj[i]);
+}
+console.log(num([6, 9, 3]));
